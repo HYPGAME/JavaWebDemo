678276758b220c36cf5fd91dda02339bbfe9c133
yp.huang <yp.huang@ctrip.com>
2018-08-08 18:05:02 +0800

    代码到了PDF第53页

--- src/main/java/com/hyp/charpter1/service/CustomerService.java
+++ src/main/java/com/hyp/charpter1/service/CustomerService.java
@@ -6,14 +6,41 @@ package com.hyp.charpter1.service;
  */
 
 import com.hyp.charpter1.model.Customer;
+import com.hyp.charpter1.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * 提供客户数据服务
  */
 public class CustomerService {
+
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
+
 
     /**
      * 获取客户列表
